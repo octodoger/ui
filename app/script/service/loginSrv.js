fb42d114e8eed5497499abb786158caeab7bfd0b
anonymous <anonymous@anonymous.anon>
2015-09-23 21:37:05 +0300

    Use 4 spaces

--- app/script/service/loginSrv.js
+++ app/script/service/loginSrv.js
@@ -1,8 +1,8 @@
 angular.module('hqcode').factory('LoginSrv', [ 'Github', 'GithubRepository', 'GithubOAuth', '$http', function (Github, GithubRepository, GithubOAuth, $http) {
-  return {
-    login: function (token) {
-      localStorage.setItem('token', token);
-      $http.defaults.headers.common['token'] = token;
-    }
-  };
+    return {
+        login: function (token) {
+            localStorage.setItem('token', token);
+            $http.defaults.headers.common['token'] = token;
+        }
+    };
 }]);
